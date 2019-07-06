798e46adf5f106f185976f14b0be92f794585b09
Christian Hill <xn.hill@gmail.com>
2019-07-06 18:07:39 +0100

    Add updated collision.py file

--- collision.py
+++ collision.py
@@ -18,6 +18,7 @@ class Particle:
         self.r = np.array((x, y))
         self.v = np.array((vx, vy))
         self.radius = radius
+        self.mass = self.radius**2
 
         self.styles = styles
         if not self.styles:
@@ -68,26 +69,14 @@ class Particle:
 
         self.r += self.v * dt
 
-        # Make the Particles bounce off the walls
-        if self.x - self.radius < 0:
-            self.x = self.radius
-            self.vx = -self.vx
-        if self.x + self.radius > 1:
-            self.x = 1-self.radius
-            self.vx = -self.vx
-        if self.y - self.radius < 0:
-            self.y = self.radius
-            self.vy = -self.vy
-        if self.y + self.radius > 1:
-            self.y = 1-self.radius
-            self.vy = -self.vy
-
 class Simulation:
     """A class for a simple hard-circle molecular dynamics simulation.
 
     The simulation is carried out on a square domain: 0 <= x < 1, 0 <= y < 1.
 
     """
+
+    ParticleClass = Particle
 
     def __init__(self, n, radius=0.01, styles=None):
         """Initialize the simulation with n Particles with radii radius.
@@ -101,6 +90,27 @@ class Simulation:
         """
 
         self.init_particles(n, radius, styles)
+        self.dt = 0.01
+
+    def place_particle(self, rad, styles):
+        # Choose x, y so that the Particle is entirely inside the
+        # domain of the simulation.
+        x, y = rad + (1 - 2*rad) * np.random.random(2)
+        # Choose a random velocity (within some reasonable range of
+        # values) for the Particle.
+        vr = 0.1 * np.sqrt(np.random.random()) + 0.05
+        vphi = 2*np.pi * np.random.random()
+        vx, vy = vr * np.cos(vphi), vr * np.sin(vphi)
+        particle = self.ParticleClass(x, y, vx, vy, rad, styles)
+        # Check that the Particle doesn't overlap one that's already
+        # been placed.
+        for p2 in self.particles:
+            if p2.overlaps(particle):
+                break
+        else:
+            self.particles.append(particle)
+            return True
+        return False
 
     def init_particles(self, n, radius, styles=None):
         """Initialize the n Particles of the simulation.
@@ -125,24 +135,25 @@ class Simulation:
         self.particles = []
         for i, rad in enumerate(radius):
             # Try to find a random initial position for this particle.
-            while True:
-                # Choose x, y so that the Particle is entirely inside the
-                # domain of the simulation.
-                x, y = rad + (1 - 2*rad) * np.random.random(2)
-                # Choose a random velocity (within some reasonable range of
-                # values) for the Particle.
-                vr = 0.1 * np.random.random() + 0.05
-                vphi = 2*np.pi * np.random.random()
-                vx, vy = vr * np.cos(vphi), vr * np.sin(vphi)
-                particle = Particle(x, y, vx, vy, rad, styles)
-                # Check that the Particle doesn't overlap one that's already
-                # been placed.
-                for p2 in self.particles:
-                    if p2.overlaps(particle):
-                        break
-                else:
-                    self.particles.append(particle)
-                    break
+            while not self.place_particle(rad, styles):
+                pass
+
+    def change_velocities(self, p1, p2):
+        """
+        Particles p1 and p2 have collided elastically: update their
+        velocities.
+
+        """
+        
+        m1, m2 = p1.mass, p2.mass
+        M = m1 + m2
+        r1, r2 = p1.r, p2.r
+        d = np.linalg.norm(r1 - r2)**2
+        v1, v2 = p1.v, p2.v
+        u1 = v1 - 2*m2 / M * np.dot(v1-v2, r1-r2) / d * (r1 - r2)
+        u2 = v2 - 2*m1 / M * np.dot(v2-v1, r2-r1) / d * (r2 - r1)
+        p1.v = u1
+        p2.v = u2
 
     def handle_collisions(self):
         """Detect and handle any collisions between the Particles.
@@ -150,24 +161,7 @@ class Simulation:
         When two Particles collide, they do so elastically: their velocities
         change such that both energy and momentum are conserved.
 
-        """
-
-        def change_velocities(p1, p2):
-            """
-            Particles p1 and p2 have collided elastically: update their
-            velocities.
-
-            """
-            
-            m1, m2 = p1.radius**2, p2.radius**2
-            M = m1 + m2
-            r1, r2 = p1.r, p2.r
-            d = np.linalg.norm(r1 - r2)**2
-            v1, v2 = p1.v, p2.v
-            u1 = v1 - 2*m2 / M * np.dot(v1-v2, r1-r2) / d * (r1 - r2)
-            u2 = v2 - 2*m1 / M * np.dot(v2-v1, r2-r1) / d * (r2 - r1)
-            p1.v = u1
-            p2.v = u2
+        """ 
 
         # We're going to need a sequence of all of the pairs of particles when
         # we are detecting collisions. combinations generates pairs of indexes
@@ -175,22 +169,46 @@ class Simulation:
         pairs = combinations(range(self.n), 2)
         for i,j in pairs:
             if self.particles[i].overlaps(self.particles[j]):
-                change_velocities(self.particles[i], self.particles[j])
+                self.change_velocities(self.particles[i], self.particles[j])
 
-    def advance_animation(self, dt):
+    def handle_boundary_collisions(self, p):
+        """Bounce the particles off the walls elastically."""
+
+        if p.x - p.radius < 0:
+            p.x = p.radius
+            p.vx = -p.vx
+        if p.x + p.radius > 1:
+            p.x = 1-p.radius
+            p.vx = -p.vx
+        if p.y - p.radius < 0:
+            p.y = p.radius
+            p.vy = -p.vy
+        if p.y + p.radius > 1:
+            p.y = 1-p.radius
+            p.vy = -p.vy
+
+    def apply_forces(self):
+        """Override this method to accelerate the particles."""
+        pass
+
+    def advance_animation(self):
         """Advance the animation by dt, returning the updated Circles list."""
 
         for i, p in enumerate(self.particles):
-            p.advance(dt)
+            p.advance(self.dt)
+            self.handle_boundary_collisions(p)
             self.circles[i].center = p.r
         self.handle_collisions()
+        self.apply_forces()
         return self.circles
 
-    def advance(self, dt):
+    def advance(self):
         """Advance the animation by dt."""
         for i, p in enumerate(self.particles):
-            p.advance(dt)
+            p.advance(self.dt)
+            self.handle_boundary_collisions(p)
         self.handle_collisions()
+        self.apply_forces()
 
     def init(self):
         """Initialize the Matplotlib animation."""
@@ -203,16 +221,11 @@ class Simulation:
     def animate(self, i):
         """The function passed to Matplotlib's FuncAnimation routine."""
 
-        self.advance_animation(0.01)
+        self.advance_animation()
         return self.circles
 
-    def do_animation(self, save=False):
-        """Set up and carry out the animation of the molecular dynamics.
-
-        To save the animation as a MP4 movie, set save=True.
-        """
-
-        fig, self.ax = plt.subplots()
+    def setup_animation(self):
+        self.fig, self.ax = plt.subplots()
         for s in ['top','bottom','left','right']:
             self.ax.spines[s].set_linewidth(2)
         self.ax.set_aspect('equal', 'box')
@@ -220,18 +233,29 @@ class Simulation:
         self.ax.set_ylim(0, 1)
         self.ax.xaxis.set_ticks([])
         self.ax.yaxis.set_ticks([])
-        anim = animation.FuncAnimation(fig, self.animate, init_func=self.init,
-                               frames=800, interval=2, blit=True)
+
+    def save_or_show_animation(self, anim, save, filename='collision.mp4'):
         if save:
             Writer = animation.writers['ffmpeg']
-            writer = Writer(fps=100, bitrate=1800)
-            anim.save('collision.mp4', writer=writer)
+            writer = Writer(fps=10, bitrate=1800)
+            anim.save(filename, writer=writer)
         else:
             plt.show()
 
+    def do_animation(self, save=False, interval=1, filename='collision.mp4'):
+        """Set up and carry out the animation of the molecular dynamics.
+
+        To save the animation as a MP4 movie, set save=True.
+        """
+
+        self.setup_animation()
+        anim = animation.FuncAnimation(self.fig, self.animate,
+                init_func=self.init, frames=800, interval=interval, blit=True)
+        self.save_or_show_animation(anim, save, filename)
+
 
 if __name__ == '__main__':
-    nparticles = 50
+    nparticles = 20
     radii = np.random.random(nparticles)*0.03+0.02
     styles = {'edgecolor': 'C0', 'linewidth': 2, 'fill': None}
     sim = Simulation(nparticles, radii, styles)
